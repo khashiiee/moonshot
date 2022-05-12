8a0f18743d4ab1c02dc884f1026e7e163e7e975a
Kavindi Peiris <kavindi.peiris@pearson.com>
2022-05-12 12:14:09 +0530

    fixed fetcher

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,10 +1,12 @@
-const fetcher = (...args) =>
-  fetch(...args).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      throw new Error(res.statusText);
-    }
-  });
+function fetcher(...urls) {
+  const f = (u) => fetch(u).then((r) => r.json());
+
+  if (urls.length > 1) {
+    return Promise.all(urls.map(f));
+  }
+  return f(urls);
+}
 
 export default fetcher;
+
+
